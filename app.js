ecd40410298e8908fa06e08a8f96f4bbde9c7ce0
kundharam sai vamshi <156309354+kundharamvamshi@users.noreply.github.com>
2026-03-26 23:15:27 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+
+const taskRouter=require('./routes/tasks');
+
+const app = express();
+
+const pool = require('./config/db');
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/v1/tasks', taskRouter);
+
+app.post('/api/v1/register', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    const isuserExists=await pool.query(`SELECT * FROM users WHERE email = $1`, [email]); 
+
+    if (isuserExists.rows.length > 0) {
+        return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const hasedPassword=await bcrypt.hash(password,10)
+    const newUserQuery =await pool.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`, [username, email, hasedPassword]);
+
+    res.status(201).json('User created successfully');
+});
+
+
+app.post('/api/v1/login', async (req,res)=>{
+    try{
+    const {email,password}=req.body;
+    const user=await pool.query(`SELECT * FROM users WHERE email = $1`, [email]);
+
+    if(user.rows.length===0){
+        return res.status(400).json({error:'Email does not exist'});
+    }
+
+      
+    
+        const validPassword=await bcrypt.compare(password,user.rows[0].password);
+        if(!validPassword){
+            return res.status(400).json({error:'Invalid email or password'});
+        }
+
+        const token=jwt.sign({userId:user.rows[0].id, role:user.rows[0].role},process.env.JWT_SECRET,{expiresIn:'12h'});
+        res.status(200).json({message:'Login successful', token});
+    
+}
+catch (error){
+    res.status(500).json({error:'Internal server error'});
+}
+});
+
+
+
+module.exports = app;
---
+++ routes/tasks.js
@@ -0,0 +1,86 @@
+const express=require('express');
+
+
+const pool=require('../config/db');
+const authenticateToken=require('../middleware/auth');  
+
+
+const router=express.Router();
+
+router.post('/', authenticateToken, async (req, res) => {
+    const { title, description } = req.body;
+    const userId = req.user.userId;
+    try {
+        const newTask = await pool.query(
+            `INSERT INTO tasks (title, description, user_id) VALUES ($1, $2, $3) RETURNING *`,[title, description, userId]
+        );
+        res.status(201).json(newTask.rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+
+router.get('/',authenticateToken,async (req,res)=>{
+    try {
+        let tasks;
+
+        if (req.user.role==='admin') {
+            tasks=await pool.query(`SELECT * FROM tasks`);
+        } else if (req.user.role==='user') {
+            tasks=await pool.query('SELECT * FROM tasks WHERE user_id=$1',[req.user.userId]);
+        }
+        res.status(200).json(tasks.rows);
+    }
+    catch (err){
+        res.status(500).json({error:'Internal server error'});
+    }
+});
+
+router.put('/:id',authenticateToken, async (req,res)=>{
+    const {id}=req.params;
+    const {title,description}=req.body;
+
+    try{
+        const task=await pool.query(`SELECT * FROM tasks WHERE id=$1`,[id]);
+
+        if (task.rows.length===0){
+            return res.status(404).json({error:'Task not found'});
+        }
+        if (task.rows[0].user_id!==req.user.userId && req.user.role!=='admin'){
+            return res.status(403).json({error:'Access denied'});
+        }
+
+        const updatedTask=await pool.query(`UPDATE tasks SET title=$1, description=$2 WHERE id=$3 RETURNING *`,[title,description,id]);
+        res.status(200).json(updatedTask.rows[0]);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+
+router.delete('/:id',authenticateToken,async (req,res)=>{
+    const {id}=req.params;
+    try{
+        const task=await pool.query(`SELECT * FROM tasks WHERE id=$1`,[id]);
+
+        if (task.rows.length===0){
+            return res.status(404).json({error:'Task not found'});
+        }
+
+        if (task.rows[0].user_id!==req.user.userId && req.user.role!=='admin'){
+            return res.status(403).json({error:'Access denied'});
+        }
+
+        await pool.query(`DELETE FROM tasks WHERE id=$1`,[id]);
+        res.status(200).json({message:'Task deleted successfully'});
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+module.exports = router;
